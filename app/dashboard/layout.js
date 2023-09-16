c9cb9b5ae7ce7686f96b4b69215bf078783e44fd
Ojigbo Leo <ojigboleo@gmail.com>
2023-09-16 16:44:24 +0100

    Auth to Dashboard

--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -1,13 +1,15 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import Logo from "../components/Logo";
 
 export default function RootLayout({ children }) {
+  const nav = useRouter();
   return (
     <html lang="en">
       <body className={"bg-[#F9FAFC]"}>
         <nav className="w-[150px] border-r min-h-screen fixed bg-white p-2">
-          <h1>Remind</h1>
+          <h1 onClick={() => nav("/")}>Remind</h1>
         </nav>
         <div className="ml-[150px]">{children}</div>
       </body>
