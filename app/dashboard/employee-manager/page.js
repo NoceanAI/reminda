948bf7d091606feee6b88d0887bf8bf7f4582796
Ojigbo Leo <ojigboleo@gmail.com>
2023-09-16 22:21:29 +0100

    update 777

---
+++ app/dashboard/employee-manager/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EmployeeManager = () => {
+  return <div className="p-2">EmployeeManager</div>;
+};
+
+export default EmployeeManager;
--- app/dashboard/layout.js
+++ app/dashboard/layout.js
@@ -6,10 +6,35 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={"bg-[#F9FAFC]"}>
-        <nav className="w-[150px] border-r min-h-screen fixed bg-white p-2">
-          <h1 onClick={() => nav("/")}>Remind</h1>
+        <nav className="w-[150px] border-r min-h-screen fixed bg-[#2C44EC] text-white p-2">
+          <h1
+            className="cursor-pointer text-[16px] font-bold"
+            onClick={() => nav.push("/")}
+          >
+            Remind
+          </h1>
+
+          <div className="mt-[50px]">
+            <p
+              className="text-[12px] cursor-pointer"
+              onClick={() => nav.push("/dashboard")}
+            >
+              Dashboard
+            </p>
+
+            <h4 className="mt-[50px] text-[14px] font-bold">Modules</h4>
+            <p
+              className="text-[12px] cursor-pointer"
+              onClick={() => nav.push("/dashboard/employee-manager")}
+            >
+              Employee Manager
+            </p>
+          </div>
         </nav>
-        <div className="ml-[150px]">{children}</div>
+        <div className="ml-[150px]">
+          <div className="h-[60px] border-b"></div>
+          {children}
+        </div>
       </body>
     </html>
   );
